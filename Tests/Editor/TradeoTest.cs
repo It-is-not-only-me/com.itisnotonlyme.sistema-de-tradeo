06f98229626e26139dcbf6822c7264e486902cfe
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-07-01 18:37:47 -0300

    Empezando la idea

---
+++ Tests/Editor/TradeoTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using ItIsNotOnlyMe.SistemaDeTradeo;
+
+public class TradeoTest
+{
+    [Test]
+    public void Test01()
+    {
+
+    }
+}
